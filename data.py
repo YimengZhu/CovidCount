af0019f52b2a59e90439d5c3641c8129c179db0e
Praktikum Projekt 12-2 <prak12-2@i81-gpu-server.irf.uni-karlsruhe.de>
2020-06-09 23:02:51 +0200

    newest version

--- data.py
+++ data.py
@@ -1,8 +1,18 @@
 from scipy.io import loadmat
+import tensorflow as tf
 import numpy as np
-import cv2
 import glob
 import os
+import cv2
+
+def preprocess_image(image):
+    image = tf.image.decode_jpeg(image)
+    image = tf.cast(image,tf.float32)
+    image /= 255.0
+    return image
+def load_and_preprocess_image(path):
+    image = tf.io.read_file(path)
+    return preprocess_image(image)
 
 class MallDataset():
     def __init__(self, data_folder, class_num):
@@ -12,17 +22,17 @@ class MallDataset():
         label_file = os.path.join(data_folder, 'mall_gt.mat')
         annotation = loadmat(label_file)
         self.count = annotation['count']
-        self.position = annotation['frame'].unsqueeze(0)
+        self.position = np.expand_dims(annotation['frame'],0)
 
-        max_count, min_count = numpy.amax(self.count), numpy.amin(self.count)
+        max_count, min_count = np.amax(self.count), np.amin(self.count)
         self.class_distance = (max_count - min_count) // class_num
 
-
-    def _generate_heatmap(self, index, height, weight):
-        heatmap = np.zeros((height, weight))
-        for i in self.position[index][0][0][0]:
-            x, y = p[0], p[1]
-            heatmap[x,y] += 1
+    def _generate_heatmap(self, index, height, width):
+        heatmap = np.zeros((height, width))
+        for i in self.position[0][0][index][0]:
+            for p in i[0]:
+                x, y = int(np.round(p[1])),int(np.round(p[0]))
+                heatmap[x][y] += 1
         heatmap = cv2.GaussianBlur(heatmap, (15, 15), 0)
         return heatmap
 
@@ -39,13 +49,13 @@ class MallDataset():
 
             batch_input_img, batch_des_img, batch_des_cls = [], [], []
             for idx in range(start_idx, start_idx + batch_size):
-                input_img = cv.imread(self.image_files[idx])
-                des_img = _generate_heatmap(idx, input_img.size[:-1])
-                des_cls = count[idx] // self.class_distance
+                input_img = load_and_preprocess_image(self.image_files[idx])
+                des_img = self._generate_heatmap(idx,input_img.shape[0],input_img.shape[1])
+                des_cls = self.count[idx] // self.class_distance
                 batch_input_img.append(input_img)
                 batch_des_img.append(des_img)
                 batch_des_cls.append(des_cls)
             start_idx += batch_size
-            yield np.array(batch_input_img), np.array(batch_des_img), np.array(batch_des_cls)
+            yield np.array(batch_input_img), [np.array(batch_des_cls),np.array(batch_des_img)]
 
 
